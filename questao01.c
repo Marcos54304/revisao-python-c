de29487060cef66a2a52a20965cc0fa5fb7369c5
Marcos54304 <marcos.00000864380@unicap.br>
2026-08-21 18:00:22 +0000

    Em andamento

---
+++ questao01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+int a, b, soma;
+
+printf("Primeiro valor: ");
+scanf ("%d", &a);
+
+printf("Segundo valor: ");
+scanf ("%d", &b );
+
+soma = a + b;
+printf ("soma: ");
+
+printf ("Soma: %d", soma);
+
+    return 0;
+}
